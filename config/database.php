365562546c3236e0d3a3ed05a3ddb041257d14dc
salah-bel <belasalah@gmail.com>
2025-04-14 14:45:25 +0200

    mise en place de l'archi

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=job_portal;charset=utf8";
+$username="root";
+$password="root";
+
+try {
+    // tente si ca marche
+    $pdo = new PDO( $dsn, $username, $password);
+    echo "connexion réussie !";
+} 
+catch(PDOException $e){
+    // si jamais ca casse tu fait ca (intercepte l'erreur)
+    echo "Connexion échouée!" . $e->getMessage();
+} 
---
+++ views/pages/homepage.html.php
@@ -0,0 +1,6 @@
+<?php require_once '../partials/header.html.php'; ?>
+
+<h1>Nos 3 dernieres offres </h1>
+
+
+<?php require_once '../partials/footer.html.php';?>
